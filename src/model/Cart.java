28022546b58bff637560b248e0662fa4aff7e09a
Jomana Mahmoud Tantawy <140981371+JomanaMahmoud@users.noreply.github.com>
2025-07-05 01:27:00 +0300

    cart class complete

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -9,7 +9,7 @@ public class Cart {
     private Map<Product, Integer> items;
 
     public Cart(Map<Product, Integer> initialItems) {
-        this.items = new LinkedHashMap<>(initialItems); // Create a copy for safety
+        this.items = new LinkedHashMap<>(initialItems);
     }
 
     public Cart(){
@@ -18,9 +18,23 @@ public class Cart {
 
     public void add(Product product, int quantity){
         if(quantity <= 0){
+            System.err.println("Error: quantity must be greater than 0");
+            return;
+        }
+        int quantityAlreadyAdded = this.items.getOrDefault(product,0);
+
+        int desiredTotalQuantity = quantity + quantityAlreadyAdded;
+
+        if(desiredTotalQuantity > product.getQuantity()){
+            System.err.println("Error: Cannot add " + quantity + " of '" + product.getName() + "' to cart. " +
+                            "You are trying to add " + desiredTotalQuantity + " but only " +
+                            product.getQuantity() + " are available in stock."
+            );
             return;
         }
         this.items.put(product, this.items.getOrDefault(product, 0) + quantity);
+        System.out.println("Success: Added " + quantity + " of '" + product.getName() + "' to the cart.");
+
     }
 
     public Map<Product, Integer> getItems(){
